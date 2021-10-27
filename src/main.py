cd8c3578d45565570f052a57e00f904eaadc439f
Tom Sasser <tsasser05@gmail.com>
2021-10-26 23:52:15 -0400

    Added TOML config containing gas, delay and key

--- src/main.py
+++ src/main.py
@@ -1,31 +1,42 @@
 import requests
-import pprint 
+import pprint
+import toml
+import time
 
-#Request with EthGasStation API key.
-#You will need to create an account and input key in the string below.
-r = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=YOURAPIKEYHERE')
+# Read the config.toml file and set variables
+f = open("/Users/tom/git/Python-Gas/src/config.toml", "r")
+config_data = toml.load(f)
+f.close()
 
-#Capture the json from request.
+gas = config_data["gas"]
+delay = config_data["delay"]
+key = config_data["key"]
+
+# Request with EthGasStation API key.
+# You will need to create an account and input key in the string below.
+url = "https://ethgasstation.info/api/ethgasAPI.json?api-key=" + key
+r = requests.get(url)
+
+# Capture the json from request.
 x = r.json()
-#Print average gas from the json obs.
+# Print average gas from the json obs.
 print("Current gas average :" + str(x['average']))
 
-###TODO
-###Define criteria for which the script will run in the background
-###Every hour? Every 30 minuets? Every 15 minuets?
-###This will help determine if this should run as daemon, a job or
-###be ran via executable.
-###Configure while loop below to accomodate for how this script will be ran.
+# TODO
+# Define criteria for which the script will run in the background
+# Every hour? Every 30 minuets? Every 15 minuets?
+# This will help determine if this should run as daemon, a job or
+# be ran via executable.
+# Configure while loop below to accomodate for how this script will be ran.
 
 
-#This is an infinite loop. It will continuously execute in it's current form
-#unless configured otherwise. 
+# This is an infinite loop. It will continuously execute in it's current form
+# unless configured otherwise.
 while(1):
-    r = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=YOURAPIKEYHERE')
+    time.sleep(delay)
+
+    r = requests.get(url)
     x = r.json()
     gas = x['average']
-    if gas <= 650:
+    if gas <= gas:
         print("Alert! Curent gas is LOW: " + str(x['average']))
-
-###TODO
-###Send alert to window pop-up
